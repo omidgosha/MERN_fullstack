11ba2c3f78671c06e3fcaaa0f383a38a7be8719f
Omid <ochehrehgosha@gmail.com>
2020-03-15 15:37:12 +0100

    initial one

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('API Running'))
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const connectDB = require ('./config/db');
+
+const app = express();
+
+//connect database
+connectDB();
+
+// initialize middleware
+app.use(express.json({extended: false}));
+
+// define routes
+app.use('/api/users', require('./api/users'));
+app.use('/api/auth', require('./api/auth'));
+app.use('/api/posts', require('./api/posts'));
+app.use('/api/profile', require('./api/profile'));
+
+app.get('/', (req, res) => res.send('API Running'));
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
